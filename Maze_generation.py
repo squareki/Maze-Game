8f5ecc63d6c311faadc1f277c313d0b7461953e9
Ilhom Khalimov <ikhalimi1@gmail.com>
2021-01-28 22:22:21 +0300

    Revert "Merge branch 'dev' of https://github.com/IPPK93/Maze-Game into dev"
    This reverts commit 503ac12d1486e3e5b0c20b997fe2042e8cf73b22, reversing
    changes made to bb2c6cb2371918c80f13d5f28b5c8fbc5db6c216.

--- Maze_generation.py
+++ Maze_generation.py
@@ -1,25 +1,25 @@
 import random
 from enum import Enum
- 
+
 class Direction(Enum):
     __order__ = "UP DOWN LEFT RIGHT"
     UP = 1
     DOWN = 2
     LEFT = 3
     RIGHT = 4
- 
- 
+    
+
 class disjoint_set_union():
     def __init__(self, size):
         self.parent = [i for i in range(size)]
         self.size = [i for i in range(size)]
- 
+        
     def find(self, v):
         if self.parent[v] == v:
             return v
         self.parent[v] = self.find(self.parent[v])
         return self.parent[v]
- 
+    
     def union(self, u, v):
         u = self.find(u)
         v = self.find(v)
@@ -28,16 +28,16 @@ class disjoint_set_union():
                 u, v = v, u
             self.parent[u] = v
             self.size[v] += self.size[u]
- 
- 
+                
+                
 class Maze():
     class Bitset():
         def __init__(self):
             self.directions = 0
- 
+        
         def __contains__(self, direction):
             return self.directions & (1 << direction)
- 
+        
         def __str__(self):
             res = []
             if not Direction.DOWN.value in self:
@@ -49,29 +49,29 @@ class Maze():
             else:
                 res.append(' ')
             return ''.join(res)
- 
+        
         def add_direction(self, direction):
             self.directions |= (1 << direction)
- 
+    
     def print_maze(self):
         [print(' _', end = '') for i in range(self.maze_width)]
         for row in self.grid:
             print('\n', '│', sep = '', end = '')
             for elem in row:
                 print(elem, end = '')
- 
+    
     def build_maze(self):
         edge_graph = []
- 
+        
         for i in range(self.maze_height):
             for j in range(self.maze_width - 1):
                 edge_graph.append((j + i * self.maze_width, j + 1 + i * self.maze_width))
         for i in range(self.maze_height - 1):
             for j in range(self.maze_width):
                 edge_graph.append((j + i * self.maze_width, j + (i + 1) * self.maze_width))
- 
+        
         random.shuffle(edge_graph)
- 
+        
         for u, v in edge_graph:
             if self.disjoint_set.find(u) != self.disjoint_set.find(v):
                 self.disjoint_set.union(u, v)
@@ -79,14 +79,14 @@ class Maze():
                     self.grid[u // self.maze_width][u % self.maze_width].add_direction(Direction.RIGHT.value)
                 else:
                     self.grid[u // self.maze_width][u % self.maze_width].add_direction(Direction.DOWN.value)                
- 
+    
     def __init__(self, maze_height, maze_width):
         self.maze_height = maze_height
         self.maze_width = maze_width
         self.grid = [[self.Bitset() for j in range(maze_width)] for i in range(maze_height)]
         self.disjoint_set = disjoint_set_union(maze_height * maze_width)
         self.build_maze()
- 
- 
+    
+
 m = Maze(20, 20)
 # m.print_maze()

ff3a908a828b53ce84931c706e3d4139dcd2df75
IPPK93 <karimov.pd@phystech.edu>
2021-01-29 20:03:31 +0300

    Complete drawing borders + fix indexes in draw_maze()

--- graphics.py
+++ graphics.py
@@ -1,18 +1,18 @@
 import pygame
-from maze_generation import *
-
+# from maze_generation import *
 
 def draw_maze(maze, surface):
-    maze.print_maze()
     cell_size = 30
     color = (0, 244, 244)
-    border_color = (0, 0, 0)
+    border_color = (10, 10, 10)
     padding = 10
     
     pygame.draw.rect(surface, border_color, (0, 0, padding, padding))
-#     pygame.draw.rect(surface, border_color, (padding, 0, (maze.width + padding) * cell_size,  ))
-    for i in range(maze.width):
-        for j in range(maze.height):
+    pygame.draw.rect(surface, border_color, (padding, 0, maze.width * (cell_size + padding), padding))
+    pygame.draw.rect(surface, border_color, (0, padding, padding,maze.height * (cell_size + padding)))
+
+    for i in range(maze.height):
+        for j in range(maze.width):
             cell = maze.grid[i][j]
             cell_x, cell_y = padding + (padding + cell_size) * j, padding + (padding + cell_size) * i
             pygame.draw.rect(surface, color, (cell_x, cell_y, cell_size, cell_size))
@@ -31,7 +31,7 @@ def draw_maze(maze, surface):
 
             
     pygame.display.update()
-
+    
 WINDOW_HEIGHT = 600
 WINDOW_WIDTH = 600
 
@@ -42,7 +42,7 @@ screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 screen.fill(BLUE)
 running = True
 
-m = Maze(10, 10)
+m = Maze(8, 12)
 
 draw_maze(m, screen)
 
